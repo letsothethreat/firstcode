9fd1c4a122b6b9a8003e2700010592460f699feb
Student <Student@LabVM2047644.wtdmxwxkjv5ulcm4xan4eivoub.jnbx.internal.cloudapp.net>
2026-04-11 23:23:26 +0200

    first commit

---
+++ src/main/java/com/mycompany/registrationloginsystem/RegistrationLoginSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registrationloginsystem;
+
+/**
+ *
+ * @author Student
+ */
+public class RegistrationLoginSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
