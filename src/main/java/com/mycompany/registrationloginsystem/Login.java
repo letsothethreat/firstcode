0b4c32f80f894ba61b690430937e6ea3de329d2c
Student <Student@LabVM2047644.wtdmxwxkjv5ulcm4xan4eivoub.jnbx.internal.cloudapp.net>
2026-04-13 13:16:19 +0200

    first commit (edited)

--- src/main/java/com/mycompany/registrationloginsystem/Login.java
+++ src/main/java/com/mycompany/registrationloginsystem/Login.java
@@ -1,41 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.registrationloginsystem;
 
 /**
  *
  * @author Student
  */
-class Login {
+public class Login {
+    String firstName;
+    String lastName;
+    String username;
+    String password;
+    String cellPhoneNumber;
+    
+  //
+    public Login(String firstName, String lastName, String username, String password, String cellPhoneNumber)  {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.cellPhoneNumber = cellPhoneNumber;
+  }
+    
+  //this is my check username method
+    public boolean checkUserName() {
+    return username.contains("_") & username.length() <=5;
+    
+  }
+    
+  //Password must be at least 8 charactersc, contains uppercase, digits and special character
+    public boolean checkPasswordComplexity() {
+  //This method is used to check if the passowrds meets all the requirements    
+    if (password.length() <8) 
+        return false;
+    
+    boolean hasCapital = false;
+    boolean hasNumber = false;
+    boolean hasSpecial = false;
+    
+    for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) hasCapital = true;
+            else if (Character.isDigit(ch)) hasNumber = true;
+            else if (!Character.isLetterOrDigit(ch)) hasSpecial = true;
+            
+        
+  } 
+      return hasUpper && hasDigit && hasSpecial;
+      
+     
+   }
+    
+  //This is my check cellphone number method
+    public boolean checkCellPhoneNumber() {
+        if(!cellPhoneNumber.startsWith("+27")) && phoneNumber.length() >=12;
+      
 
-    boolean checkPasswordComplexity(String password) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    boolean checkPasswordComplexixity(String password) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    boolean checkCellPhoneNumber(String cellPhonenumber) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    String registerUser(String username, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    boolean loginUser(String loginUsername, String loginPassword) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    boolean checkUserName(String username) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    boolean returnLoginStatus(boolean loginResult) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
+  //This is my register user method
+    public String registerUser() {
+        if(checkUserName() && checkPasswordComplexity() && checkCellPhoneNumber()) {
+      return "Registration successful for user: " + username;
+      
+      } else {
+      return "Registration failed.Please try again" ;
+      
+    }
+    }
+    
+  //This is my return login status method
+    public String returnLoginStatus(String enterUsername, String enteredPassword) {
 }
